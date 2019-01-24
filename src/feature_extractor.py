dd84d970dda8086588e894d57c27dd1105c4f925
we <david@wemakeprice.com>
2019-01-24 17:04:12 +0900

    check clustering for feature from trained model

--- src/feature_extractor.py
+++ src/feature_extractor.py
@@ -385,102 +385,13 @@ def main(args):
     fig.savefig("tsne-unsupervised_20_class.png")
 
 
-# def run_epoch(model, loss_fn, loader, optimizer, dtype, epoch):
-#     """
-#     Train the model for one epoch.
-#     """
-#     # Set the model to training mode
-#     model.train()
-#     mean_loss=0
-#     for x, y in loader:
-#         # The Dataloader produces Torch Tensors, so we need to cast them to the
-#         # correct datatype and wrap them in Variables.
-#         #
-#         # Note that the labels should be a torch.LongTensor on CPU and a
-#         # torch.cuda.LongTensor on GPU; to accomplish this we first cast to dtype
-#         # (either torch.FloatTensor or torch.cuda.FloatTensor) and then cast to
-#         # long; this ensures that y has the correct type in both cases.
-#         # x_var = Variable(x.type(dtype))
-#         x_var = Variable(x.cuda())
-#         y_var = Variable(y.type(dtype).long())
-#
-#         # Run the model forward to compute scores and loss.
-#         scores = model(x_var)
-#         loss = loss_fn(scores, y_var)
-#         mean_loss += loss
-#         # Run the model backwrad and take a step using the optimizer.
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
-#
-#     size_batch_per_epoch = (len(loader)/args.batch_size)
-#     print("mean_loss : ", mean_loss.item()/size_batch_per_epoch)
-#
-#     if epoch % args.num_per_epoch == 0:
-#         torch.save({
-#             'epoch': epoch,
-#             'model_state_dict': model.state_dict(),
-#             'optimizer_state_dict': optimizer.state_dict(),
-#             'loss': loss,
-#         }, args.save_dir_trained_model+str(epoch)+name_pretrained_model)
-#
-#
-#
-#
-# def check_accuracy(model, loader, dtype):
-#     """
-#     Check the accuracy of the model
-#     """
-#     # Set the model to eval mode
-#     model.eval()
-#     num_correct, num_samples = 0, 0
-#
-#     for x, y in loader:
-#         # Cast the image data to the correct type and wrap it in a Variable. At
-#         # test-time when we do not need to compute gradients, marking the Variable
-#         # as volatile can reduce memory usage and slightly improve speed.
-#         x_var = Variable(x.cuda(), volatile=True)
-#
-#         # Run the model forward, and compare the argmasx score with ground-truth category
-#         scores = model(x_var)
-#         _, preds = scores.data.cpu().max(1)
-#         num_correct += (preds == y).sum()
-#         num_samples += x.size(0)
-#
-#
-#     # Return the fraction of datapoints that were correctly classfied.
-#     acc = float(num_correct)/ num_samples
-#     return acc
-
 
 
 
 if __name__ == '__main__':
     args = parser.parse_args()
     main(args)
-    # print(args.save_dir_trained_model)
-    # model = resnet50(pretrained=True)
-    # model.eval()
-    # model.fc = nn.Linear(model.fc.in_features, 20)
-    # optimizer = torch.optim.Adam(model.fc.parameters(), lr=1e-5)
-    #
-    #
-    # check_point = torch.load(args.save_dir_trained_model+"19600"+name_pretrained_model)
-    # print("check_point :", check_point)
-    #
-    #
-    # args.num_epoch1 = check_point['epoch']
-    #
-    # model.load_state_dict(check_point["model_state_dict"])
-    # optimizer.load_state_dict(check_point["optimizer_state_dict"])
-    # loss = check_point["loss"]
-    #
-    # print(args.num_epoch1)
-    # print(loss)
-    #
-    #
-    # for (i, param) in enumerate(model.fc.parameters()):
-    #     print(i, param)
+
 
 
 
